c026eec533020416335a29b90fd3aec2b8435706
dohe0342 <kimdohe1070@gmail.com>
2023-06-12 02:24:36 +0900

    from local

--- egs/tedlium3/ASR/local/compute_fbank_tedlium_pseudo.py
+++ egs/tedlium3/ASR/local/compute_fbank_tedlium_pseudo.py
@@ -103,5 +103,6 @@ if __name__ == "__main__":
     formatter = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
 
     logging.basicConfig(format=formatter, level=logging.INFO)
+    spk_id = sys.argv[1]
 
     compute_fbank_tedlium()
