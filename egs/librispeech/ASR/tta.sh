3dc1ceeeda30a4c09e3e1ad73b5e625c3e99628d
dohe0342 <kimdohe1070@gmail.com>
2023-06-23 01:32:55 +0900

    from local

--- egs/librispeech/ASR/tta.sh
+++ egs/librispeech/ASR/tta.sh
@@ -1,39 +1,32 @@
 spk_id=$1
 dir=$2
-#for i in 100 200 300
-for i in 50
-#for i in 10 20 30 40 50 60 70 80 90 100
-#for i in 50 100 150 200 250 300 350 400 450 500
-#for i in 50 100 #150 200 250 300 350 400 450 500
-#for i in 127800 127850
+
+
+for method in greedy_search
 do
-	#for method in modified_beam_search
-	for method in greedy_search
-	do
-	  ./tta/decode.py \
-		--input-strategy AudioSamples \
-		--enable-spec-aug False \
-		--additional-block True \
-		--exp-dir ./pruned_transducer_stateless_d2v_v2/$2 \
-		--max-duration 600 \
-		--iter $i \
-		--model-name ../d2v-base-T.pt \
-		--decoding-method $method \
-		--max-sym-per-frame 1 \
-		--encoder-type d2v \
-		--encoder-dim 768 \
-		--decoder-dim 768 \
-		--joiner-dim 768 \
-		--avg 1 \
-		--use-averaged-model True \
-		--spk-id $spk_id \
-		--prompt False \
-		--manifest-dir /DB/data/fbank \
-		--bpe-model /DB/data/lang_bpe_500/bpe.model \
-		--res-name test
-		#--res-name bitfit_q_fc1_check$i
-		#--res-name fullft_check$i
-	done
+  ./tta/decode.py \
+	--input-strategy AudioSamples \
+	--enable-spec-aug False \
+	--additional-block True \
+	--exp-dir ./pruned_transducer_stateless_d2v_v2/$2 \
+	--max-duration 600 \
+	--iter $i \
+	--model-name ../d2v-base-T.pt \
+	--decoding-method $method \
+	--max-sym-per-frame 1 \
+	--encoder-type d2v \
+	--encoder-dim 768 \
+	--decoder-dim 768 \
+	--joiner-dim 768 \
+	--avg 1 \
+	--use-averaged-model True \
+	--spk-id $spk_id \
+	--prompt False \
+	--manifest-dir /DB/data/fbank \
+	--bpe-model /DB/data/lang_bpe_500/bpe.model \
+	--res-name test
+	#--res-name bitfit_q_fc1_check$i
+	#--res-name fullft_check$i
 done
 
 #--prompt True \
