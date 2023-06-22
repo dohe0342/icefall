80781e41f27a2b5cccfb3cca1549d92153284423
dohe0342 <kimdohe1070@gmail.com>
2023-06-23 00:35:20 +0900

    from local

--- egs/librispeech/ASR/prepare.sh
+++ egs/librispeech/ASR/prepare.sh
@@ -302,7 +302,7 @@ if [ $stage -le 11 ] && [ $stop_stage -ge 11 ]; then
     ./local/prepare_lm_training_data.py \
       --bpe-model $lang_dir/bpe.model \
       --lm-$manifest_dir $dl_dir/lm/librispeech-lm-norm.txt \
-      --lm-archive $out_dir/lm_$manifest_dir.pt
+      --lm-archive $out_dir/lm_data.pt
   done
 fi
 
