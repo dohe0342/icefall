8d8df874bfb1cb124dd22bac2ae1679f9861525e
dohe0342 <kimdohe1070@gmail.com>
2023-06-23 13:29:36 +0900

    from local

---
+++ egs/librispeech/ASR/tta_suta_all.sh
@@ -0,0 +1,15 @@
+#dl_dir=/DB/LibriSpeech_tar/vox
+dl_dir=/DB/LibriSpeech
+subset=$1
+res_name=$2
+#dl_dir=/home/work/workspace/LibriSpeech/vox_v3
+
+for dest in $subset; do
+#for dest in "test-clean" "test-other"; do
+	for spk in $dl_dir/$dest/*; do
+		spk_id=${spk#*$dest\/}
+		echo $spk_id
+		./tta.sh $spk_id $res_name
+	done
+done
+
