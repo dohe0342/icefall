b29d5765ed1d1d28f7147feb740bd83a2cc3e041
dohe0342 <kimdohe1070@gmail.com>
2023-06-28 01:53:30 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_transf/decode.py
+++ egs/librispeech/ASR/pruned_transducer_transf/decode.py
@@ -476,7 +476,7 @@ def decode_one_batch(
             simulate_streaming=True,
         )
     else:
-        if params.use_transf_pred::
+        if params.use_transf_pre:
             encoder_out, encoder_out_lens, mask = model.encoder(x=feature, x_lens=feature_lens, return_mask=True)
         else:
             encoder_out, encoder_out_lens = model.encoder(x=feature, x_lens=feature_lens)
