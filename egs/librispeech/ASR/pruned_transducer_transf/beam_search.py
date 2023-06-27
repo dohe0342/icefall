dce020c7f78d73f125bcf7430e8efbab14e7f385
dohe0342 <kimdohe1070@gmail.com>
2023-06-28 02:54:26 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_transf/beam_search.py
+++ egs/librispeech/ASR/pruned_transducer_transf/beam_search.py
@@ -592,6 +592,7 @@ def greedy_search(
         current_encoder_out = encoder_out[:, t:t+1, :].unsqueeze(2)
         # fmt: on
         decoder_out = decoder_out[0, -1, :].unsqueeze(0)
+        print(current_encoder_out.size(), decoder_out.size())
         logits = model.joiner(
             current_encoder_out, decoder_out.unsqueeze(1), project_input=False
         )
