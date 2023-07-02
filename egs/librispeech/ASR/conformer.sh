6c2c174abb5768cb885e5c2b2cd7a196d39579ed
dohe0342 <kimdohe1070@gmail.com>
2023-07-02 18:58:55 +0900

    from local

--- egs/librispeech/ASR/conformer.sh
+++ egs/librispeech/ASR/conformer.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 ./pruned_transducer_stateless5/train.py \
-  --world-size 8 \
+  --world-size 4 \
   --num-epochs 30 \
   --start-epoch 1 \
   --full-libri 1 \
@@ -14,5 +14,6 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
   --nhead 8 \
   --encoder-dim 512 \
   --decoder-dim 512 \
-  --joiner-dim 512
+  --joiner-dim 512 \
+  --start-epoch 30
 
