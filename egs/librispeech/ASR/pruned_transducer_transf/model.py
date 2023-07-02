fccbb34f91943f12e17017f5bdba2afb4630a49e
dohe0342 <kimdohe1070@gmail.com>
2023-07-02 18:20:36 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_transf/model.py
+++ egs/librispeech/ASR/pruned_transducer_transf/model.py
@@ -208,12 +208,13 @@ class Transducer(nn.Module):
         # project_input=False since we applied the decoder's input projections
         # prior to do_rnnt_pruning (this is an optimization for speed).
         logits = self.joiner(am_pruned, lm_pruned, project_input=False)
-        for b, logit in enumerate(logits):
-            for t, l in enumerate(logit):
-                #print(l.size())
-                print(l[0].argmax(), l[1].argmax(), l[2].argmax(), l[3].argmax(), l[4].argmax())
-                print(y_padded[b])
-            exit()
+        if logits.device.index == 0:
+            for b, logit in enumerate(logits):
+                for t, l in enumerate(logit):
+                    #print(l.size())
+                    print(l[0].argmax(), l[1].argmax(), l[2].argmax(), l[3].argmax(), l[4].argmax())
+                    print(y_padded[b])
+                exit()
 
         with torch.cuda.amp.autocast(enabled=False):
             pruned_loss = k2.rnnt_loss_pruned(
