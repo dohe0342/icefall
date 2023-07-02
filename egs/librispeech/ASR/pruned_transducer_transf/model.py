972071078552b8a4279e018e9107128db7129b8b
dohe0342 <kimdohe1070@gmail.com>
2023-07-02 18:16:02 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_transf/model.py
+++ egs/librispeech/ASR/pruned_transducer_transf/model.py
@@ -210,8 +210,8 @@ class Transducer(nn.Module):
         logits = self.joiner(am_pruned, lm_pruned, project_input=False)
         for b, logit in enumerate(logits):
             for t, l in enumerate(logit):
-                print(l.size())
-                print(l.argmax())
+                #print(l.size())
+                print(l[0].argmax(), l[1].argmax(), l[2].argmax(), l[3].argmax(), l[4].argmax())
                 print(y_padded[b])
             exit()
 
