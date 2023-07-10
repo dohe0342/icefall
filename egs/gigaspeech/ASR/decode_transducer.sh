4e108869dbf1274f8bfb9aabd9d1a77282d6e2f8
dohe0342 <kimdohe1070@gmail.com>
2023-07-10 23:47:22 +0900

    from local

--- egs/gigaspeech/ASR/decode_transducer.sh
+++ egs/gigaspeech/ASR/decode_transducer.sh
@@ -1,16 +1,16 @@
 # greedy search
 ./pruned_transducer_stateless2/decode.py \
+  --avg 1
   --epoch 20 \
   --decoding-method greedy_search \
   --exp-dir pruned_transducer_stateless2/exp \
   --bpe-model data/lang_bpe_500/bpe.model \
   --max-duration 600 \
-  --avg 1
 
 # fast beam search
 ./pruned_transducer_stateless2/decode.py \
-  --iter 3488000 \
-  --avg 20 \
+  --avg 1
+  --epoch 20 \
   --decoding-method fast_beam_search \
   --exp-dir pruned_transducer_stateless2/exp \
   --bpe-model data/lang_bpe_500/bpe.model \
