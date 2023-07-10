703ade4c3b7c8ec59fc3e46d9d7c865056a2b9c2
dohe0342 <kimdohe1070@gmail.com>
2023-07-10 23:27:07 +0900

    from local

--- egs/gigaspeech/ASR/run.sh
+++ egs/gigaspeech/ASR/run.sh
@@ -1,7 +1,7 @@
 ./conformer_ctc/decode.py \
   --epoch 20 \
   --avg 1 \
-  --method attention-decoder \
+  --method 1best \
   --num-paths 1000 \
   --exp-dir conformer_ctc/exp \
   --lang-dir data/lang_bpe_500 \
