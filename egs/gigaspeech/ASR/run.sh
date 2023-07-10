2ea2568aac41a3210091f558bab5c3103aed93bc
dohe0342 <kimdohe1070@gmail.com>
2023-07-10 23:09:58 +0900

    from local

--- egs/gigaspeech/ASR/run.sh
+++ egs/gigaspeech/ASR/run.sh
@@ -3,7 +3,7 @@
   --avg 1 \
   --method attention-decoder \
   --num-paths 1000 \
-  --exp-dir conformer_ctc/exp_500 \
+  --exp-dir conformer_ctc/exp \
   --lang-dir data/lang_bpe_500 \
   --max-duration 20 \
   --num-workers 1
