8e7e03454997cbe445064086c961426960138562
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 13:05:22 +0900

    from local

--- egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/conformer.py
+++ egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/conformer.py
@@ -149,11 +149,8 @@ class Conformer(EncoderInterface):
         if prompt is not None:
             prompt = prompt.expand((x.size()[0], prompt.size()[0], prompt.size()[1]))
             x = torch.cat([prompt, x], dim=1)
+            x_lens += prompt.size()[1]
 
-        print('*'*30)
-        print(x_lens)
-        print('*'*30)
-        
         x = x.permute(1, 0, 2)  # (N, T, C) -> (T, N, C)
 
         # Caution: We assume the subsampling factor is 4!
