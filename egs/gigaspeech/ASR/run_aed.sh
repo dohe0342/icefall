845afd698d940da31c60eddf8837061a5cf37ffc
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 15:59:17 +0900

    from local

--- egs/gigaspeech/ASR/run_aed.sh
+++ egs/gigaspeech/ASR/run_aed.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 ./conformer_ctc/train_prompt.py \
   --max-duration 600 \
-  --num-workers 1 \
+  --num-workers 9 \
   --world-size 8 \
   --exp-dir conformer_ctc/exp_500 \
   --lang-dir data/lang_bpe_500
