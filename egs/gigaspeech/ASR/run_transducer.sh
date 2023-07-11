d301193dcfb2b4ab4dc44330a84dab32150a466d
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 14:04:18 +0900

    from local

--- egs/gigaspeech/ASR/run_transducer.sh
+++ egs/gigaspeech/ASR/run_transducer.sh
@@ -9,5 +9,5 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
   --num-epochs 3 \
   --prompt True \
   --input-strategy PrecomputedFeatures \
-  --initial-lr 0.0001 \
+  --initial-lr 0.01 \
   --use-fp16 True
