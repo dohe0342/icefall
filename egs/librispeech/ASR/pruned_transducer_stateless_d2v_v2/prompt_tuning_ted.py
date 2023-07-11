9d41edd21016667bc3712a1fd7646c102d9a4ede
dohe0342 <kimdohe1070@gmail.com>
2023-07-12 00:46:33 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning_ted.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning_ted.py
@@ -100,7 +100,7 @@ from torch import Tensor
 from torch.cuda.amp import GradScaler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
-from zipformer import Zipformer
+#from zipformer import Zipformer
 from data2vec_encoder import FairSeqData2VecEncoder
 
 from icefall import diagnostics
