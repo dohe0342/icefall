84325505aac0b81b72cea128ba075470313c8b58
dohe0342 <dohe1070@gmail.com>
2023-07-11 19:58:11 +0900

    from local

--- egs/gigaspeech/ASR/decode_transducer.sh
+++ egs/gigaspeech/ASR/decode_transducer.sh
@@ -6,8 +6,8 @@
   --decoding-method greedy_search \
   --exp-dir pruned_transducer_stateless2_prompt/$1 \
   --bpe-model data/lang_bpe_500/bpe.model \
-  --max-duration 600 \
-  --prompt True 
+  --max-duration 600 
+  #--prompt True 
   #--input-strategy PrecomputedFeatures
 # fast beam search
 #./pruned_transducer_stateless2/decode.py \
