0804336b09af152836944bcd329937f4cb6f941e
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 15:58:48 +0900

    from local

---
+++ egs/gigaspeech/ASR/run_aed.sh
@@ -0,0 +1,13 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+./pruned_transducer_stateless2_prompt/train_prompt.py \
+  --max-duration 600 \
+  --num-workers 9 \
+  --world-size 4 \
+  --exp-dir pruned_transducer_stateless2_prompt/$1 \
+  --model-name ../epoch-15.pt \
+  --bpe-model data/lang_bpe_500/bpe.model \
+  --num-epochs 5 \
+  --prompt True \
+  --input-strategy PrecomputedFeatures \
+  --initial-lr 0.001 \
+  --use-fp16 True
