7784e6843f6e83837b0595503f19e85cb03c8ce8
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 12:49:38 +0900

    from local

--- egs/gigaspeech/ASR/run_transducer.sh
+++ egs/gigaspeech/ASR/run_transducer.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 ./pruned_transducer_stateless2/train.py \
   --max-duration 600 \
-  --num-workers 1 \
+  --num-workers 9 \
   --world-size 8 \
   --exp-dir pruned_transducer_stateless2/exp \
   --bpe-model data/lang_bpe_500/bpe.model \
