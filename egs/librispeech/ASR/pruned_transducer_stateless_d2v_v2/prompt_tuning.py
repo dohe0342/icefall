8e0163a44a0b6ed51ba7bf12cb7d0915df97eca0
dohe0342 <kimdohe1070@gmail.com>
2023-07-12 02:27:10 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
@@ -88,6 +88,7 @@ import torch
 import torch.multiprocessing as mp
 import torch.nn as nn
 from asr_datamodule import LibriSpeechAsrDataModule
+from asr_datamodule_ted2 import TedLiumAsrDataModule
 from decoder import Decoder
 from joiner import Joiner
 from lhotse.cut import Cut
