73db415d9aaeb16d93f33a095f19b1e095b87465
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 14:18:03 +0900

    from local

--- egs/gigaspeech/ASR/decode_transducer.sh
+++ egs/gigaspeech/ASR/decode_transducer.sh
@@ -9,21 +9,21 @@
   --input-strategy PrecomputedFeatures
 
 # fast beam search
-./pruned_transducer_stateless2/decode.py \
-  --avg 1 \
-  --epoch 20 \
-  --decoding-method fast_beam_search \
-  --exp-dir pruned_transducer_stateless2/exp \
-  --bpe-model data/lang_bpe_500/bpe.model \
-  --input-strategy PrecomputedFeatures \
-  --max-duration 600
-
-# modified beam search
-./pruned_transducer_stateless2/decode.py \
-  --avg 1 \
-  --epoch 15 \
-  --decoding-method modified_beam_search \
-  --exp-dir pruned_transducer_stateless2/exp \
-  --bpe-model data/lang_bpe_500/bpe.model \
-  --input-strategy PrecomputedFeatures \
-  --max-duration 600
+#./pruned_transducer_stateless2/decode.py \
+#  --avg 1 \
+#  --epoch 20 \
+#  --decoding-method fast_beam_search \
+#  --exp-dir pruned_transducer_stateless2/exp \
+#  --bpe-model data/lang_bpe_500/bpe.model \
+#  --input-strategy PrecomputedFeatures \
+#  --max-duration 600
+#
+## modified beam search
+#./pruned_transducer_stateless2/decode.py \
+#  --avg 1 \
+#  --epoch 15 \
+#  --decoding-method modified_beam_search \
+#  --exp-dir pruned_transducer_stateless2/exp \
+#  --bpe-model data/lang_bpe_500/bpe.model \
+#  --input-strategy PrecomputedFeatures \
+#  --max-duration 600
