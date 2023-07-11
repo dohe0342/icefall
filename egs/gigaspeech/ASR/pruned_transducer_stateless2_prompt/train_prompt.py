ad10d9b49c69417fa06520a225709f04a8fe572a
dohe0342 <kimdohe1070@gmail.com>
2023-07-11 14:03:35 +0900

    from local

--- egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/train_prompt.py
+++ egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/train_prompt.py
@@ -829,8 +829,6 @@ def run(rank, world_size, args):
             else:
                 p.requries_grad = False
     
-    exit()
-
     optimizer = Eve(model_params, lr=params.initial_lr)
     scheduler = Eden(optimizer, params.lr_batches, params.lr_epochs)
 
