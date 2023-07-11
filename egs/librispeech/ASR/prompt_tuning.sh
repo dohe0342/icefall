2fcb164426e33263d5f7b89a5aa31b7db4c7c405
dohe0342 <kimdohe1070@gmail.com>
2023-07-12 02:05:47 +0900

    from local

--- egs/librispeech/ASR/prompt_tuning.sh
+++ egs/librispeech/ASR/prompt_tuning.sh
@@ -28,6 +28,7 @@ if [ $workstation = "whale" ]; then
 			--encoder-type d2v \
 			--additional-block True \
 			--prune-range 10 \
+			--spk-id $2
 			--prompt True
 		#touch ./pruned_transducer_stateless_d2v_v2/$1/.train.done
 	fi
