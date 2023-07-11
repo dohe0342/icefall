ec73c7c765c9e252e567ee2a3f474a5e34cd5eca
dohe0342 <dohe1070@gmail.com>
2023-07-11 21:22:47 +0900

    from local

--- egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/conformer.py
+++ egs/gigaspeech/ASR/pruned_transducer_stateless2_prompt/conformer.py
@@ -154,7 +154,7 @@ class Conformer(EncoderInterface):
         if prompt is not None:
             prompt = prompt.expand((x.size()[0], prompt.size()[0], prompt.size()[1]))
             x = torch.cat([prompt, x], dim=1)
-            print(pos_emb)
+            print(pos_emb.size())
             exit()
 
         
