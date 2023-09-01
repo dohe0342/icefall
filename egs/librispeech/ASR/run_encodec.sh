9a5cbf959bbf2e3d3bfc6c60f162766bf58fecb5
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 16:37:56 +0900

    from local

--- egs/librispeech/ASR/run_encodec.sh
+++ egs/librispeech/ASR/run_encodec.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
   --full-libri 1 \
   --exp-dir pruned_transducer_stateless5/exp-L \
   --max-duration 450 \
-  --use-fp16 0 \
+  --use-fp16 1 \
   --num-encoder-layers 18 \
   --dim-feedforward 2048 \
   --nhead 8 \
