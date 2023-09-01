e200fdd93b4bb84f4cf42f10948175f7c37527c8
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 13:52:15 +0900

    from local

---
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/encodec.py
@@ -0,0 +1,35 @@
+from datasets import load_dataset, Audio
+from transformers import EncodecModel, AutoProcessor
+import numpy as np
+import soundfile as sf
+
+# load a demonstration datasets
+librispeech_dummy = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+
+# load the model + processor (for pre-processing the audio)
+model = EncodecModel.from_pretrained("facebook/encodec_24khz")
+processor = AutoProcessor.from_pretrained("facebook/encodec_24khz")
+
+# cast the audio data to the correct sampling rate for the model
+librispeech_dummy = librispeech_dummy.cast_column("audio", Audio(sampling_rate=processor.sampling_rate))
+audio_sample = librispeech_dummy[0]["audio"]["array"]
+sf.write('original.wav', np.array(audio_sample), 24000)
+
+# pre-process the inputs
+inputs = processor(raw_audio=audio_sample, sampling_rate=processor.sampling_rate, return_tensors="pt")
+
+# explicitly encode then decode the audio inputs
+encoder_outputs = model.encode(inputs["input_values"], inputs["padding_mask"], bandwidth=24)
+print(encoder_outputs.keys())
+print(encoder_outputs.audio_codes.size())
+print(encoder_outputs.audio_codes[0][0][0][0])
+
+codebook = model.quantizer.decode(encoder_outputs.audio_codes[0])
+print(codebook.size())
+print(codebook)
+audio_values = model.decode(encoder_outputs.audio_codes, encoder_outputs.audio_scales, inputs["padding_mask"])[0]
+
+# or the equivalent with a forward pass
+audio_values = model(inputs["input_values"], inputs["padding_mask"]).audio_values
+audio_values = audio_values.detach().numpy()[0][0]
+sf.write('encodec_24kb_test.wav', audio_values, 24000)
