8b467019197e5266e10c5723956021d1eceff68b
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 17:11:16 +0900

    from local

--- egs/librispeech/ASR/encodec/train.py
+++ egs/librispeech/ASR/encodec/train.py
@@ -666,7 +666,7 @@ def compute_loss(
         padding_mask = torch.ones(feature.size())
         for i, length in enumerate(feature_lens):
             length = int(length.item())
-            padding_mask[i][:
+            padding_mask[i][length:] = 0
         feature_idx = encodec.encode(feature, inputs["padding_mask"], bandwidth=24)
         feature_idx = feature_idx.audio_codes[0].transpose(0, 1)
         feature = encodec.quantizer.decode(feature_idx)
