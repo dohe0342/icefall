ad83943cd2f5196a95da4e2fb14ea0d8c500deda
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 16:37:36 +0900

    from local

---
+++ egs/librispeech/ASR/run_encodec.sh
@@ -0,0 +1,16 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+
+./pruned_transducer_stateless5/train.py \
+  --world-size 8 \
+  --num-epochs 30 \
+  --start-epoch 1 \
+  --full-libri 1 \
+  --exp-dir pruned_transducer_stateless5/exp-L \
+  --max-duration 300 \
+  --use-fp16 0 \
+  --num-encoder-layers 18 \
+  --dim-feedforward 2048 \
+  --nhead 8 \
+  --encoder-dim 512 \
+  --decoder-dim 512 \
+  --joiner-dim 512
