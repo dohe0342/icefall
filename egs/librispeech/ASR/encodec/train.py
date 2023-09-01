d05552e92971ce50699ecc58a276299148b7ab55
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 17:01:53 +0900

    from local

--- egs/librispeech/ASR/encodec/train.py
+++ egs/librispeech/ASR/encodec/train.py
@@ -625,7 +625,7 @@ def compute_loss(
     batch: dict,
     is_training: bool,
     warmup: float = 1.0,
-    encodec,
+    encodec=None,,
 ) -> Tuple[Tensor, MetricsTracker]:
     """
     Compute RNN-T loss given the model and its inputs.
