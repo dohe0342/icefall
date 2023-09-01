d93c427b2d2b54c7079cdd268089bb53011dafb7
dohe0342 <kimdohe1070@gmail.com>
2023-09-01 13:59:13 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/encodec.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/encodec.py
@@ -3,6 +3,7 @@ from transformers import EncodecModel, AutoProcessor
 import numpy as np
 import soundfile as sf
 
+bandwidth = 1.5
 # load a demonstration datasets
 librispeech_dummy = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
 
