9596995dbd3493b04d8ac0c552d1ce15e6c3057b
dohe0342 <kimdohe1070@gmail.com>
2023-09-04 12:10:40 +0900

    from local

--- egs/librispeech/ASR/encodec/conformer.py
+++ egs/librispeech/ASR/encodec/conformer.py
@@ -160,9 +160,7 @@ class Conformer(EncoderInterface):
         x = x.permute(1, 0, 2)  # (N, T, C) -> (T, N, C)
 
         #lengths = (((x_lens - 1) >> 1) - 1) >> 1
-        print(x_lens)
-        exit()
-        lengths = (((x_lens - 1) >> 1) - 1) >> 1
+        lengths = (x_lens // 320)
         #lengths = x.size(0)
         #assert x.size(0) == lengths.max().item()
         src_key_padding_mask = make_pad_mask(lengths)
