3e24fd0f109b0d7ae277a677a5cfab2858e474ec
dohe0342 <kimdohe1070@gmail.com>
2023-09-05 19:51:12 +0900

    from local

---
+++ egs/librispeech/ASR/run_conformer.sh
@@ -0,0 +1,19 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+./encodec/train.py \
+  --world-size 4 \
+  --num-epochs 30 \
+  --start-epoch 1 \
+  --full-libri 1 \
+  --initial-lr 0.0001 \
+  --input-strategy AudioSamples \
+  --enable-spec-aug False \
+  --exp-dir encodec/test \
+  --max-duration 150 \
+  --use-fp16 1 \
+  --num-encoder-layers 18 \
+  --dim-feedforward 2048 \
+  --nhead 8 \
+  --encoder-dim 512 \
+  --decoder-dim 512 \
+  --joiner-dim 512
