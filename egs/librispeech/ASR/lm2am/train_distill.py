1417ff9329559dcabbf31586a5225ce6296ed7f2
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 16:40:14 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -728,13 +728,13 @@ def compute_loss(
                 allow_truncate=params.subsampling_factor - 1,
             )
             
-            print(nnet_output.size())
-            print(supervision_segments)
-            print(decoding_graph)
-            print(params.use_double_scores)
-            print(token_ids)
-            print(supervisions["text"])
-            exit()
+            #print(nnet_output.size())
+            #print(supervision_segments)
+            #print(decoding_graph)
+            #print(params.use_double_scores)
+            #print(token_ids)
+            #print(supervisions["text"])
+            #exit()
 
             ctc_loss = k2.ctc_loss(
                 decoding_graph=decoding_graph,
