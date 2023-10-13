cafbfb93bda101cdae14867af8e16fa158cba5f2
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 16:52:39 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -293,7 +293,8 @@ class Conformer(Transformer):
             lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
             #lm_am_sim = F.log_softmax(lm_am_sim / 3, dim=-1)
             lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
-            lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
+            #lm_am_sim = lm_am_sim.transpose(0, 1).contiguous()
+            lm_am_sim = lm_am_sim.continuous()
             print('3', lm_am_sim.size())
 
             ##############################
