fb3a792366857a44d253d445abb3d6b866571068
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 17:21:31 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -290,6 +290,7 @@ class Conformer(Transformer):
             alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
             alignment_target = [[int(j+1) for j in range(alignment_lengths[i])] for i in range(len(alignment_lengths))]
             print('1', alignment_lengths)
+            print('2', alignment_target)
             '''
             if 0:
                 alignment_lengths -= 1
