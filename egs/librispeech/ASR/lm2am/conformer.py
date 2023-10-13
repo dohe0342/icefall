d36c083e57bc02998a01402b246d207a6e1be83b
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 16:22:12 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -33,6 +33,7 @@ from scaling import (
 )
 from subsampling import Conv2dSubsampling
 from transformer import Supervisions, Transformer, encoder_padding_mask
+from transformers import GPT2Tokenizer, GPT2Model, BertTokenizer, BertModel
 
 
 class Conformer(Transformer):
