f13e7e2c507a0ef02d995ea988b033e79db7727d
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 17:31:34 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -727,7 +727,7 @@ def compute_loss(
                 use_double_scores=params.use_double_scores,
             )
 
-
+            ctc_loss = ctc_loss + 0.1*distill_loss
 
         if not params.interctc and not params.condition and not params.distill:
             if type(nnet_output) == tuple:
