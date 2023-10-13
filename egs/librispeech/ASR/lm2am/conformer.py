11dfd3b710bd53425f45e893d1e74449a87cd26b
dohe0342 <kimdohe1070@gmail.com>
2023-10-13 17:24:12 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -310,7 +310,7 @@ class Conformer(Transformer):
             #############for alignment target ###############################
             
             x = self.ctc_output(encoder_memory)
-            return x, encoder_memory, memory_key_padding_mask
+            return (x, lm_am_sim, alignment_target), encoder_memory, memory_key_padding_mask
         else:
             x = self.ctc_output(encoder_memory)
             return x, encoder_memory, memory_key_padding_mask
