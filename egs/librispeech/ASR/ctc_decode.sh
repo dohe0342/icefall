39915e6a464477767ed189d9821c419be2169b34
dohe0342 <kimdohe1070@gmail.com>
2023-10-14 11:30:10 +0900

    from local

--- egs/librispeech/ASR/ctc_decode.sh
+++ egs/librispeech/ASR/ctc_decode.sh
@@ -1,5 +1,5 @@
 for method in ctc-greedy-search ctc-decoding 1best nbest-oracle; do
   python3 ./conformer_ctc2/decode.py \
   --exp-dir conformer_ctc2/exp \
-  --use-averaged-model True --epoch 18 --avg 1 --max-duration 200 --method $method
+  --use-averaged-model False --epoch 18 --avg 1 --max-duration 200 --method $method
 done
