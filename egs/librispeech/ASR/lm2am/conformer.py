4996c970eb8d5ea52f91317cace82ff8dcb5ec50
김도희 <dohee@macmini.local>
2023-10-15 20:51:56 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -280,6 +280,8 @@ class Conformer(Transformer):
             am_output = encoder_memory.transpose(0, 1)
             if 1:
                 print('1', am_output.size())
+                print('2', x.size())
+                print('3', x.argmax(dim=-1).size())
                 am_output = am_output[x.argmax(dim=-1) != 0]
                 print('2', am_output.size())
             am_output = self.distill_linear(am_output)
