cb568fd669eec686e7be4ff8d7fa1e9fa6b43eb5
김도희 <dohee@macmini.local>
2023-10-15 20:20:26 +0900

    from local

--- egs/librispeech/ASR/ctc_decode.sh
+++ egs/librispeech/ASR/ctc_decode.sh
@@ -1,5 +1,6 @@
 for method in ctc-greedy-search ctc-decoding 1best nbest-oracle; do
   python3 ./lm2am/decode.py \
   --exp-dir lm2am/conformer-18layer-256dim_gpt2-small \
+  --result-dir lm2am/conformer-18layer-256dim_gpt2-small \
   --use-averaged-model False --epoch 30 --avg 10 --max-duration 200 --method $method
 done
