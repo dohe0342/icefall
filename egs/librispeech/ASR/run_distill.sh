4030f1f339b78efd5c1fd5d5deb1eca515a51c26
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 11:25:11 +0900

    from local

---
+++ egs/librispeech/ASR/run_distill.sh
@@ -0,0 +1,13 @@
+    WORLD_SIZE=8
+    export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+    ./conformer_ctc2/train.py \
+    --manifest-dir data/fbank \
+    --exp-dir lm2am/exp \
+    --full-libri 1 \
+    --spec-aug-time-warp-factor 80 \
+    --max-duration 600 \
+    --world-size ${WORLD_SIZE} \
+    --start-epoch 1 \
+    --num-epochs 30 \
+    --att-rate 0.0 \
+    --num-decoder-layers 0
