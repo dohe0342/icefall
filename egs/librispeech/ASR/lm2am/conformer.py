dae489774d47da68a91fd177f6e2c627ab816bb1
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 12:06:13 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -292,6 +292,7 @@ class Conformer(Transformer):
             for layer in self.lm_decoder[:-1]:
                 am_output = layer(am_output)
             am_output = am_output.transpose(1, 2)
+            am_output = self.lm_decoder[-1](am_output)
             #am_output = F.normalize(am_output, dim=2) 
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
 
