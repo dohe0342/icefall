44c2d70d1889a9db9eec7680529506e80d656306
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 12:38:01 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -6,11 +6,5 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
   --start-epoch 1 \
   --full-libri 1 \
   --exp-dir pruned_transducer_stateless5/test \
-  --max-duration 300 \
-  --use-fp16 1 \
-  --num-encoder-layers 18 \
-  --dim-feedforward 2048 \
-  --nhead 8 \
-  --encoder-dim 512 \
-  --decoder-dim 512 \
-  --joiner-dim 512
+  --max-duration 600 \
+  --use-fp16 1
