045172fa1df6fc10a19d8a55c129d3a0ddc6368e
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 10:57:21 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -297,6 +297,7 @@ class Conformer(Transformer):
             #############for alignment target ###############################
             alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
             alignment_target = [[int(j+1) for j in range(alignment_lengths[i])] for i in range(len(alignment_lengths))]
+            
             alignment_flat = torch.linspace(
                                                 1,
                                                 alignment_lengths[0],
