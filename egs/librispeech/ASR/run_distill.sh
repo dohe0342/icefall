536e18908714087889cbf2a844d955e5bc8dd8b2
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 17:13:27 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -10,7 +10,7 @@
     --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
-    --num-epochs 30 \
+    --num-epochs 60 \
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--distill True \
