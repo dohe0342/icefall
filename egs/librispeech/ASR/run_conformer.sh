e59522808b8114e3ce0bddf6b30310c0464c932d
김도희 <dohee@macmini.local>
2023-11-14 19:20:06 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -3,14 +3,14 @@
     ./conformer_ctc2/train.py \
     --manifest-dir data/fbank \
     --exp-dir conformer_ctc2/$1 \
-    --full-libri 0 \
+    --full-libri 1 \
 	--use-fp16 True \
 	--num-workers 9 \
     --spec-aug-time-warp-factor 80 \
     --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
-    --num-epochs 60 \
+    --num-epochs 30 \
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--master-port 12355
