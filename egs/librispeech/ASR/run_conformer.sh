17ec4bc1896f13d609b35227caab39e6b752c93a
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 12:59:41 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -1,10 +1,18 @@
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
-
-./pruned_transducer_stateless5/train.py \
-  --world-size 4 \
-  --num-epochs 30 \
-  --start-epoch 1 \
-  --full-libri 1 \
-  --exp-dir pruned_transducer_stateless5/test \
-  --max-duration 600 \
-  --use-fp16 1
+    WORLD_SIZE=4
+    export CUDA_VISIBLE_DEVICES="4,5,6,7"
+    ./lm2am/train_distill.py \
+    --manifest-dir data/fbank \
+    --exp-dir lm2am/$1 \
+    --full-libri 1 \
+	--use-fp16 True \
+	--num-workers 9 \
+    --spec-aug-time-warp-factor 80 \
+    --max-duration 1200 \
+    --world-size ${WORLD_SIZE} \
+    --start-epoch 1 \
+    --num-epochs 30 \
+    --att-rate 0.0 \
+    --num-decoder-layers 0 \
+	--distill True \
+	--distill-rate 0.1 \
+	--unused-params True
