beb864bf95ce74172569b4e5aa6705dcf8f8d07b
김도희 <dohee@macmini.local>
2023-11-14 21:43:26 +0900

    from local

--- egs/librispeech/ASR/lm2am/decode.py
+++ egs/librispeech/ASR/lm2am/decode.py
@@ -904,6 +904,7 @@ def main() -> None:
 
     model.to(device)
     model.eval()
+    model.distill = False
     num_param = sum([p.numel() for p in model.parameters()])
     logging.info(f"Number of model parameters: {num_param}")
 
