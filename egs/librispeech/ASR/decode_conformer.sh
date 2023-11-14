fb77bd76ea81eb5ac7d2d7592b7425be317a68c4
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 17:31:13 +0900

    from local

--- egs/librispeech/ASR/decode_conformer.sh
+++ egs/librispeech/ASR/decode_conformer.sh
@@ -1,7 +1,7 @@
 python3 ./conformer_ctc2/decode.py \
 	--exp-dir $1 \
-	--use-averaged-model False \
-	--epoch 31 \
-	--avg 1 \
+	--use-averaged-model True \
+	--epoch 57 \
+	--avg 8 \
 	--max-duration 200 \
 	--method ctc-greedy-search
