ffb45e7d2da7aa54d7cadc40e0d2ebc612cd2690
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 12:18:34 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -708,10 +708,11 @@ def compute_loss(
                 supervision_segments,
                 allow_truncate=params.subsampling_factor - 1,
             )
+            
             alignment_graph = graph_compiler.compile(alignment_target)
             
-            print(alignment_graph)
             print(dense_fsa_vec)
+            print(alignment_graph)
 
             ctc_loss = k2.ctc_loss(
                 decoding_graph=decoding_graph,
