f0ada25a0dc6a9d7f6108d222808b2203650271e
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-14 11:31:03 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,5 +1,5 @@
-    WORLD_SIZE=8
-    export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+    WORLD_SIZE=4
+    export CUDA_VISIBLE_DEVICES="0,1,2,3"
     ./conformer_ctc2/train.py \
     --manifest-dir data/fbank \
     --exp-dir lm2am/exp \
