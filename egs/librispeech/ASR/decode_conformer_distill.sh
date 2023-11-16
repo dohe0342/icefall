92ce0c71e87f07bbec7093067f807c45530f70a3
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2023-11-16 15:45:31 +0900

    from local

--- egs/librispeech/ASR/decode_conformer_distill.sh
+++ egs/librispeech/ASR/decode_conformer_distill.sh
@@ -2,7 +2,7 @@ python3 ./lm2am/decode.py \
 	--exp-dir $1 \
 	--use-averaged-model False \
 	--epoch $2 \
-	--avg 10 \
+	--avg 1 \
 	--max-duration 200 \
 	--num-decoder-layers 0 \
 	--distill True \
