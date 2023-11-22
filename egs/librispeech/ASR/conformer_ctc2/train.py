0f22534634f78a5e167e897d53c24aa3f3f2c296
김도희 <dohee@macmini.local>
2023-11-22 10:59:33 +0900

    from local

--- egs/librispeech/ASR/conformer_ctc2/train.py
+++ egs/librispeech/ASR/conformer_ctc2/train.py
@@ -53,6 +53,7 @@ import torch
 import torch.multiprocessing as mp
 #from asr_datamodule import TedLiumAsrDataModule
 from asr_datamodule import LibriSpeechAsrDataModule
+from asr_datamodule_ted2 import TedLiumAsrDataModule
 from conformer import Conformer
 from lhotse.dataset.sampling.base import CutSampler
 from lhotse.utils import fix_random_seed
