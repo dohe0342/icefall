deb9689e0421ef3230cff43548011a358e27a5d4
김도희 <dohee@macmini.local>
2023-11-22 10:55:33 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -1,4 +1,5 @@
-    WORLD_SIZE=4
+:<< 'END'
+WORLD_SIZE=4
     export CUDA_VISIBLE_DEVICES="0,1,2,3"
     ./conformer_ctc2/train.py \
     --manifest-dir data/fbank \
@@ -14,3 +15,22 @@
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--master-port 12355
+END
+
+WORLD_SIZE=4
+    export CUDA_VISIBLE_DEVICES="0,1,2,3"
+    ./conformer_ctc2/train.py \
+    --manifest-dir /home/work/workspace/icefall/egs/tedlium2/data/fbank \
+    --lang-dir /home/work/workspace/icefall/egs/tedlium2/data/lang_bpe_500 \
+    --exp-dir conformer_ctc2/$1 \
+    --full-libri 0 \
+    --use-fp16 True \
+    --num-workers 9 \
+    --spec-aug-time-warp-factor 80 \
+    --max-duration 1200 \
+    --world-size ${WORLD_SIZE} \
+    --start-epoch 1 \
+    --num-epochs 70 \
+    --att-rate 0.0 \
+    --num-decoder-layers 0 \
+    --master-port 12355
