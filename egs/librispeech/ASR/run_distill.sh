788ed6898b384dafe37faa50c71a3ae9b53a33a5
김도희 <dohee@macmini.local>
2023-11-22 15:58:26 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,9 +1,7 @@
     WORLD_SIZE=4
     export CUDA_VISIBLE_DEVICES="4,5,6,7"
     ./lm2am/train_distill.py \
-	--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
-	--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
-	--ted2 True \
+	--ted2 False \
 	--master-port 12356 \
     --exp-dir lm2am/$1 \
     --full-libri 0 \
@@ -19,3 +17,6 @@
 	--distill True \
 	--distill-rate 0.2 \
 	--unused-params True
+	#--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
+	#--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
+
