9c73c0ba36319476b0284b9bc3f9c7b4bc854f23
김도희 <dohee@macmini.local>
2023-11-22 13:15:01 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -1242,8 +1242,6 @@ def run(rank, world_size, args):
         valid_cuts += librispeech.dev_other_cuts()
         valid_dl = librispeech.valid_dataloaders(valid_cuts)
 
-
-
     train_cuts = train_cuts.filter(remove_short_and_long_utt)
     train_cuts = train_cuts.filter(remove_invalid_utt_ctc)
 
