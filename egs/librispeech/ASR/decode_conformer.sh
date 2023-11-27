e687d0069cef9fc0484db8928ea7a051313dcc60
김도희 <dohee@macmini.local>
2023-11-27 14:22:06 +0900

    from local

--- egs/librispeech/ASR/decode_conformer.sh
+++ egs/librispeech/ASR/decode_conformer.sh
@@ -1,6 +1,7 @@
 python3 ./conformer_ctc2/decode.py \
 	--exp-dir $1 \
 	--use-averaged-model True \
+	--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
 	--epoch $2 \
 	--ted2 True \
 	--avg 10 \
