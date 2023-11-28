4814e77897b54d5e93dafb912c90c8a246195833
김도희 <dohee@macmini.local>
2023-11-28 15:59:07 +0900

    from local

--- egs/librispeech/ASR/run_l2s.sh
+++ egs/librispeech/ASR/run_l2s.sh
@@ -1,17 +1,17 @@
     WORLD_SIZE=4
-    export CUDA_VISIBLE_DEVICES="4,5,6,7"
+    export CUDA_VISIBLE_DEVICES="0,1,2,3"
     ./distill_l2s/train_distill.py \
 	--ted2 False \
-	--master-port 12355 \
+	--master-port 12356 \
     --exp-dir distill_l2s/$1 \
-    --full-libri 1 \
+    --full-libri 0 \
 	--use-fp16 True \
 	--num-workers 9 \
     --spec-aug-time-warp-factor 80 \
     --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
-    --num-epochs 40 \
+    --num-epochs 70 \
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--distill True \
