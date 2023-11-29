c476d64256a6d56fa5f7baf39b6c6ef9bc43fe3b
김도희 <dohee@macmini.local>
2023-11-29 15:56:06 +0900

    from local

--- egs/librispeech/ASR/run_l2s.sh
+++ egs/librispeech/ASR/run_l2s.sh
@@ -2,7 +2,7 @@
     export CUDA_VISIBLE_DEVICES="0,1,2,3"
     ./distill_l2s/train_distill.py \
 	--ted2 True \
-	--master-port 12355 \
+	--master-port 12357 \
 	--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
 	--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
     --exp-dir distill_l2s/$1 \
