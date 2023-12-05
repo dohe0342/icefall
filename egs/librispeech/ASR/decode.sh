5e600f626869c1fe69da4f04f31aae15d2522a6a
김도희 <dohee@dohee.local>
2023-12-05 11:36:04 +0900

    from local

--- egs/librispeech/ASR/decode.sh
+++ egs/librispeech/ASR/decode.sh
@@ -1,6 +1,6 @@
 #dl_dir=/DB/LibriSpeech
 dl_dir=/home/work/workspace/LibriSpeech
-for dest in "test-clean"; do
+for dest in "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
@@ -9,7 +9,7 @@ for dest in "test-clean"; do
 		#./test.sh $spk_id encoderfreeze_$spk_id
 		#./test.sh $spk_id encoderlast2_$spk_id
 		#./test.sh $spk_id bitfit_"$spk_id"_q_fc1
-		./test.sh $spk_id baseline
+		./test2.sh $spk_id baseline
 		#./test.sh $spk_id self_init_"$spk_id"
 		#./test.sh $spk_id prompt_tuning_"$spk_id"
 		#./test.sh $spk_id "$spk_id"_adapter_30m
