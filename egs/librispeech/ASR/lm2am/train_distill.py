e87d6c084fbf2bb5b4158801de5ae2f6f1e96692
김도희 <dohee@dohee.local>
2024-01-08 13:42:39 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -819,7 +819,7 @@ def compute_loss(
                 #    f"att_loss: {att_loss}"
                 #)
                 #display_and_save_batch(batch, params=params, sp=graph_compiler.sp)
-                #distill_loss = distill_loss[distill_loss_is_finite]
+                distill_loss = distill_loss[distill_loss_is_finite]
 
                 # If the batch contains more than 10 utterances AND
                 # if either all ctc_loss or att_loss is inf or nan,
