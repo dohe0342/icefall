60eb6b2e14950e8b6a48803f96dd4d0c829d43dc
김도희 <dohee@dohee.local>
2024-01-10 17:25:27 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,7 +1,7 @@
     WORLD_SIZE=4
-    export CUDA_VISIBLE_DEVICES="0,1,2,3"
+    export CUDA_VISIBLE_DEVICES="4,5,6,7"
     ./lm2am/train_distill.py \
-	--master-port 12355 \
+	--master-port 12356 \
     --exp-dir lm2am/$1 \
     --full-libri 1 \
 	--use-fp16 True \
@@ -9,15 +9,15 @@
     --spec-aug-time-warp-factor 80 \
     --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
-    --start-epoch 26 \
+    --start-epoch 1 \
     --num-epochs 40 \
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
-	--lm-name gpt2
-	#--lm-name mistralai/Mistral-7B-v0.1
+	--lm-name mistralai/Mistral-7B-v0.1
+	#--lm-name gpt2
 	#--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
 	#--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
 #--ted2 True \
