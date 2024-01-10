d95af0125cad7a1996ed87d8fe8914d4b8136aaa
김도희 <dohee@dohee.local>
2024-01-10 17:37:50 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -7,7 +7,7 @@
 	--use-fp16 True \
 	--num-workers 9 \
     --spec-aug-time-warp-factor 80 \
-    --max-duration 100 \
+    --max-duration 300 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
     --num-epochs 40 \
