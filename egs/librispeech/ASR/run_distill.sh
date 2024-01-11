22a2a54754c3cc815478f44476e79cfff3b41630
김도희 <dohee@dohee.local>
2024-01-11 10:57:57 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -7,7 +7,7 @@
 	--use-fp16 True \
 	--num-workers 9 \
     --spec-aug-time-warp-factor 80 \
-    --max-duration 40 \
+    --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
     --num-epochs 40 \
@@ -16,7 +16,8 @@
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
-	--lm-name mistralai/Mistral-7B-v0.1
+	--lm-name gpt2-xl
+	#--lm-name mistralai/Mistral-7B-v0.1
 	#--lm-name gpt2
 	#--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
 	#--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
