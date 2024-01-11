e64e0132cef718cf466b41e67216a30e7a36d3f8
김도희 <dohee@dohee.local>
2024-01-11 10:33:52 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -1167,6 +1167,7 @@ def run(rank, world_size, args):
     print('*'*20)
     print(len([p for n, p in model.named_paramters() if 'lm.' not in n]))
     print('*'*20)
+    
     optimizer = optim.Eve([p for n, p in model.named_paramters() if 'lm.' not in n], lr=params.initial_lr)
     scheduler = optim.Eden(optimizer, params.lr_batches, params.lr_epochs)
 
