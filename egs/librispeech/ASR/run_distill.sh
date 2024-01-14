131c08c11036f8ca57fedb34e4da4e23d86ba6ba
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-01-14 13:21:40 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,18 +1,18 @@
-    WORLD_SIZE=4
-    export CUDA_VISIBLE_DEVICES="0,1,2,3"
-    ./lm2am/train_distill.py \
+WORLD_SIZE=4
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+./lm2am/train_distill.py \
 	--master-port 12356 \
-    --exp-dir lm2am/$1 \
-    --full-libri 1 \
+	--exp-dir lm2am/$1 \
+	--full-libri 1 \
 	--use-fp16 True \
 	--num-workers 9 \
-    --spec-aug-time-warp-factor 80 \
-    --max-duration 900 \
-    --world-size ${WORLD_SIZE} \
-    --start-epoch 1 \
-    --num-epochs 40 \
-    --att-rate 0.0 \
-    --num-decoder-layers 0 \
+	--spec-aug-time-warp-factor 80 \
+	--max-duration 900 \
+	--world-size ${WORLD_SIZE} \
+	--start-epoch 1 \
+	--num-epochs 40 \
+	--att-rate 0.0 \
+	--num-decoder-layers 0 \
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
