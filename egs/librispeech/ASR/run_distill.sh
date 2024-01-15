c990643ae1f630c5b2a3bca4ccd0ab0fd43093a6
김도희 <dohee@dohee.local>
2024-01-15 12:13:57 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
-	--max-duration 900 \
+	--max-duration 1200 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
 	--num-epochs 40 \
@@ -16,7 +16,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
-	--lm-name gpt2-medium
+	--lm-name gpt2
 #--lm-name mistralai/Mistral-7B-v0.1
 #--lm-name gpt2
 #--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
