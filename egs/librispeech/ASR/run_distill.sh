f236cf83522f4bcd03d698bf55e3545941dcb7b1
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-01-16 02:29:18 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	--spec-aug-time-warp-factor 80 \
 	--max-duration 1200 \
 	--world-size ${WORLD_SIZE} \
-	--start-epoch 1 \
+	--start-epoch 16 \
 	--num-epochs 40 \
 	--att-rate 0.0 \
 	--num-decoder-layers 0 \
