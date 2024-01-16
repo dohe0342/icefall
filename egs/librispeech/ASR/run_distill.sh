50d813f4d57fd567dd52fc5a9408a43fca5357a1
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-01-17 02:12:59 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES="4,5,6,7"
 ./lm2am/train_distill.py \
 	--master-port 12354 \
 	--exp-dir lm2am/$1 \
-	--full-libri 1 \
+	--full-libri 0 \
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
