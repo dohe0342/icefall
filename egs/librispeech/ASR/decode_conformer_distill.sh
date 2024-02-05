3de57dffba79fdd638e26a2f554b256326bdda0e
김도희 <dohee@dohee.local>
2024-02-05 20:14:58 +0900

    from local

--- egs/librispeech/ASR/decode_conformer_distill.sh
+++ egs/librispeech/ASR/decode_conformer_distill.sh
@@ -2,7 +2,7 @@ python3 ./lm2am/decode.py \
 	--exp-dir $1 \
 	--use-averaged-model True \
 	--epoch $2 \
-	--avg 10 \
+	--avg 2 \
 	--max-duration 1200 \
 	--num-decoder-layers 0 \
 	--distill True \
