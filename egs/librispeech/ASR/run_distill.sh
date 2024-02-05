9c35485fbd7f20a8cbdad0cf8255baa0cb3bbb95
김도희 <dohee@dohee.local>
2024-02-06 01:54:52 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,22 +1,23 @@
 WORLD_SIZE=4
-export CUDA_VISIBLE_DEVICES="4,5,6,7"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 ./lm2am/train_distill.py \
-	--master-port 12355 \
+	--master-port 12356 \
 	--exp-dir lm2am/$1 \
-	--full-libri 1 \
+	--full-libri 0 \
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
 	--max-duration 1200 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
-	--num-epochs 40 \
+	--num-epochs 70 \
 	--att-rate 0.0 \
 	--num-decoder-layers 0 \
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
 	--quant True \
+	--kernel-size 31 \
 	--lm-name gpt2
 	#--lm-name gpt2
 #--lm-name gpt2-medium
