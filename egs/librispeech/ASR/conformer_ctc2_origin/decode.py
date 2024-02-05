72672cef99f9d471ca9dcda6b91ae5d5f8c4b813
김도희 <dohee@dohee.local>
2024-02-05 17:47:43 +0900

    from local

--- egs/librispeech/ASR/conformer_ctc2_origin/decode.py
+++ egs/librispeech/ASR/conformer_ctc2_origin/decode.py
@@ -924,9 +924,6 @@ def main():
     model.to(device)
     model.eval()
 
-    print(model)
-    exit()
-
     num_param = sum([p.numel() for p in model.parameters()])
     logging.info(f"Number of model parameters: {num_param}")
 
