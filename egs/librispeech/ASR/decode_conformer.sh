3a3ed4de967e61cd0008658f4dcef665c625f57a
김도희 <dohee@dohee.local>
2024-02-06 11:11:28 +0900

    from local

--- egs/librispeech/ASR/decode_conformer.sh
+++ egs/librispeech/ASR/decode_conformer.sh
@@ -1,10 +1,11 @@
-python3 ./conformer_ctc2_origin/decode.py \
+python3 ./conformer_ctc2/decode.py \
 	--exp-dir $1 \
 	--use-averaged-model True \
 	--epoch $2 \
 	--avg 10 \
-	--max-duration 1200 \
+	--max-duration 200 \
 	--num-decoder-layers 0 \
+	--kernel-size 31 \
 	--method ctc-greedy-search
 
 #--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
