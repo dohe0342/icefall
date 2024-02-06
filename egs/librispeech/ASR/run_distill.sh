2e01223089c6cca87e3ce933a5a05a6b99875434
김도희 <dohee@dohee.local>
2024-02-06 16:42:05 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
-	--max-duration 1200 \
+	--max-duration 900 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
 	--num-epochs 70 \
