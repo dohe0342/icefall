dee80c380863266c0e03b8619571b33769055611
김도희 <dohee@dohee.local>
2024-02-06 22:14:20 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -22,14 +22,14 @@ WORLD_SIZE=4
     ./conformer_ctc2/train.py \
     --master-port 12355 \
     --exp-dir conformer_ctc2/$1 \
-    --full-libri 0 \
+    --full-libri 1 \
     --use-fp16 True \
     --num-workers 9 \
     --spec-aug-time-warp-factor 80 \
     --max-duration 1200 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
-    --num-epochs 70 \
+    --num-epochs 40 \
     --att-rate 0.0 \
     --num-decoder-layers 0 \
 	--kernel-size 31
