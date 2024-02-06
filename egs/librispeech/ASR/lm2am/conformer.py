4bff1dfe755270943fb8187fb77b789147851b47
김도희 <dohee@dohee.local>
2024-02-06 17:14:16 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -131,7 +131,7 @@ class Conformer(Transformer):
 
         if quant:
             self.quant = GumbelVectorQuantizer(dim=d_model, 
-                                               num_vars=320, 
+                                               num_vars=160, 
                                                temp=(2, 0.5, 0.999995), 
                                                groups=8, 
                                                combine_groups=False, 
