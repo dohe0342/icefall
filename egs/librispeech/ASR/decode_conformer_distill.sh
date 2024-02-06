9f1ac71a85ed3c0c391182de5ba21a1954afc282
김도희 <dohee@dohee.local>
2024-02-07 00:23:17 +0900

    from local

--- egs/librispeech/ASR/decode_conformer_distill.sh
+++ egs/librispeech/ASR/decode_conformer_distill.sh
@@ -1,12 +1,12 @@
 python3 ./lm2am/decode.py \
 	--exp-dir $1 \
-	--use-averaged-model false \
+	--use-averaged-model True \
 	--epoch $2 \
-	--avg 1 \
+	--avg 10 \
 	--max-duration 400 \
 	--num-decoder-layers 0 \
 	--distill True \
 	--method ctc-greedy-search \
 	--quant False \
-	--kernel-size 15 \
+	--kernel-size 31 \
 	--lm-name gpt2
