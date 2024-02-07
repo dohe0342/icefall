9805d245a8fddc7c9cdd1291811454fa8c9dc003
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-07 16:16:21 +0900

    from local

--- icefall/utils.py
+++ icefall/utils.py
@@ -929,6 +929,10 @@ class MetricsTracker(collections.defaultdict):
         """
         for k, v in self.norm_items():
             tb_writer.add_scalar(prefix + k, v, batch_idx)
+        
+        for k, v in self.norm_items():
+            wb_writer.log(prefix + k, v, batch_idx)
+
 
 
 def concat(ragged: k2.RaggedTensor, value: int, direction: str) -> k2.RaggedTensor:
