e48796938bd17c2851afd50a8d95920956720be1
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-07 16:57:49 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -16,7 +16,7 @@ export CUDA_VISIBLE_DEVICES="4,5,6,7"
 	--distill True \
 	--distill-rate 0.1 \
 	--unused-params True \
-	--quant False \
+	--quant True \
 	--kernel-size 15 \
 	--lm-name gpt2
 	#--lm-name gpt2
