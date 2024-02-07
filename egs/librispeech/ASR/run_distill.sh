71230ce25517183dfca597aeaca10fdf7047984d
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-07 16:59:52 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,7 +1,7 @@
 WORLD_SIZE=4
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 ./lm2am/train_distill.py \
-	--master-port 12358 \
+	--master-port 12350 \
 	--exp-dir lm2am/$1 \
 	--full-libri 1 \
 	--use-fp16 True \
