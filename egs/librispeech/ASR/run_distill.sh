ca2c257c639f3767126f0507aafee000c48f8e17
김도희 <dohee@dohee.local>
2024-02-09 19:29:33 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,7 +1,7 @@
 WORLD_SIZE=4
-export CUDA_VISIBLE_DEVICES="4,5,6,7"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 ./lm2am/train_distill.py \
-	--master-port 12353 \
+	--master-port 12354 \
 	--exp-dir lm2am/$1 \
 	--full-libri 1 \
 	--use-fp16 True \
