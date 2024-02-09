39173a8c573517a28a0fe6ab1885f295830ee0f5
김도희 <dohee@dohee.local>
2024-02-09 15:05:45 +0900

    from local

--- egs/librispeech/ASR/lm2am/train.py
+++ egs/librispeech/ASR/lm2am/train.py
@@ -315,7 +315,7 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--average-period",
         type=int,
-        default=100,
+        default=1000,
         help="""Update the averaged model, namely `model_avg`, after processing
         this number of batches. `model_avg` is a separate version of model,
         in which each floating-point parameter is the average of all the
