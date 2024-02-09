5e5f92b68f0ebb11c5f4f93114a7d3558c79b242
김도희 <dohee@dohee.local>
2024-02-09 14:11:33 +0900

    from local

--- egs/librispeech/ASR/decode_conformer_distill.sh
+++ egs/librispeech/ASR/decode_conformer_distill.sh
@@ -1,17 +1,13 @@
-for epoch in "30" "40"; do
-	for avg in "1" "10"; do
-		python3 ./lm2am/decode_multiple.py \
-			--exp-dir $1 \
-			--use-averaged-model True \
-			--epoch $epoch \
-			--avg $avg \
-			--max-duration 1200 \
-			--num-decoder-layers 0 \
-			--distill True \
-			--method ctc-greedy-search \
-			--quant False \
-			--kernel-size 15 \
-			--lm-name gpt2 \
-			--wandb True
-	done
-done
+python3 ./lm2am/decode_multiple.py \
+	--exp-dir $1 \
+	--use-averaged-model True \
+	--epoch $epoch \
+	--avg $avg \
+	--max-duration 1200 \
+	--num-decoder-layers 0 \
+	--distill True \
+	--method ctc-greedy-search \
+	--quant False \
+	--kernel-size 15 \
+	--lm-name gpt2 \
+	--wandb True
