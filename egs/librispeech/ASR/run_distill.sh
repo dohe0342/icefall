4b3d8b5fe851fd8e42985f7f8e173bb2b3f0d5c7
김도희 <dohee@dohee.local>
2024-02-09 20:07:40 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -1,7 +1,9 @@
 WORLD_SIZE=4
 export CUDA_VISIBLE_DEVICES="4,5,6,7"
+port=$(($RANDOM% 601+12300))
 ./lm2am/train_distill.py \
 	--exp-dir lm2am/$1 \
+	--master-port $port \
 	--full-libri 1 \
 	--use-fp16 True \
 	--num-workers 9 \
