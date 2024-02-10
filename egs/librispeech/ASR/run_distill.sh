ddf42209fb4e46cac0eac021c58a25cdb212a947
김도희 <dohee@dohee.local>
2024-02-11 01:12:02 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -15,7 +15,7 @@ port=$(($RANDOM% 601+12300))
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
-	--max-duration 1200 \
+	--max-duration 900 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
 	--num-epochs 40 \
