01a5bc77a77df088f10e0d3c9dfd0efb51bd8d75
김도희 <dohee@dohee.local>
2024-02-12 15:13:21 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -765,10 +765,10 @@ def compute_loss(
                 reduction=params.reduction,
                 use_double_scores=params.use_double_scores,
             )
-            
+             
             distill_loss = k2.ctc_loss(
                 decoding_graph=alignment_graph,
-                dense_fsa_vec=dense_fsa_vec,
+                dense_fsa_vec=dense_fsa_vec_lm if params.ted2 else dense_fsa_vec,
                 output_beam=params.beam_size,
                 reduction=params.reduction,
                 use_double_scores=params.use_double_scores,
