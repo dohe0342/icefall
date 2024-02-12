0c47943a57a183d645452b2a85918e4c7a8ab88a
김도희 <dohee@dohee.local>
2024-02-12 14:30:43 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -751,7 +751,7 @@ def compute_loss(
             dense_fsa_vec_lm = k2.DenseFsaVec(
                 lm_am_sim,
                 supervision_segments_lm,
-                allow_truncate=15,
+                allow_truncate=7,
             )
 
             alignment_graph = graph_compiler.compile(alignment_target)
