efa228cc2f6ecc0ac1a4b471b9459b474b4ec8bb
김도희 <dohee@dohee.local>
2024-02-19 09:45:50 +0900

    from local

--- egs/librispeech/ASR/run_conformer.sh
+++ egs/librispeech/ASR/run_conformer.sh
@@ -34,7 +34,6 @@ WORLD_SIZE=4
 	--dim-model 512 \
 	--att-rate 0.0 \
     --num-decoder-layers 0 \
-
 	--kernel-size 31
 #--manifest-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/fbank \
 #--lang-dir /home/work/workspace/icefall/egs/tedlium2/ASR/data/lang_bpe_500 \
