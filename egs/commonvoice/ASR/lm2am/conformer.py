80f2018e2509cfa1da7c9865428fe614b194c0f7
김도희 <dohee@dohee.local>
2024-02-20 20:42:57 +0900

    from local

--- egs/commonvoice/ASR/lm2am/conformer.py
+++ egs/commonvoice/ASR/lm2am/conformer.py
@@ -135,7 +135,7 @@ class Conformer(Transformer):
                                                temp=(2, 0.5, 0.999995), 
                                                groups=2, 
                                                combine_groups=False, 
-                                               vq_dim=512, 
+                                               vq_dim=256, 
                                                time_first=True,)
         else:
             self.quant = None
