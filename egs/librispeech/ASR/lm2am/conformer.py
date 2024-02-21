8f8753e9bebe483ff0f429919105bbb81a511af7
김도희 <dohee@dohee.local>
2024-02-21 15:42:04 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -180,7 +180,6 @@ class Conformer(Transformer):
 
         self.distill = distill
         if self.distill:
-            print('fuck!!!!!!!!!')
             ########### for gpt2
             if 'bert' in lm_name:
                 self.tokenizer = BertTokenizer.from_pretrained(lm_name)
