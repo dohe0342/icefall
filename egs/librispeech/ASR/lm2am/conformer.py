908cac87e49ba2213028dbf0bd85cb0ebd9e64e6
김도희 <dohee@dohee.local>
2024-02-21 16:14:38 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -384,7 +384,9 @@ class Conformer(Transformer):
                 lm_am_sim_prob, lm_am_sim_idx = lm_am_sim_cp.max(-1)
                 lm_am_sim_bool = lm_am_sim_prob > 0.6
                 for b in range(lm_am_sim_prob.size(0)):
-                    
+                    audio_len = lm_am_sim_prob.size(2)
+                    target_len = lm_am_sim_prob.size(1)
+                    print(audio_len, target_len)
                 '''
                 file_name = str(torch.randint(1, 10000, (1,)).item())
                 for b in range(lm_am_sim_cp.size(0)):
