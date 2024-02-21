98a625f66d6f0c8f50c24bf5e623a4bf2c845e91
김도희 <dohee@dohee.local>
2024-02-21 17:04:13 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -399,7 +399,7 @@ class Conformer(Transformer):
                         while True:
                             now_alignment = idx[i].item() == alignment
                             should_plus = idx[i].item() == (alignment + 1)
-                            should_mius = idx[i].item() == (aligned_idx[-1] + 2)
+                            should_mius = idx[i+1].item() == (alignment + 1)
                             
                             if should_move:
                                 alignment += 1
