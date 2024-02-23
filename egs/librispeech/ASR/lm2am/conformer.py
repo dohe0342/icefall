344de7fa5ae8f127e8c1a10f98b2bc402eb69cd9
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-02-23 16:07:00 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -442,8 +442,8 @@ class Conformer(Transformer):
                     plt.savefig(f'./png/{file_name}/alingment{batch}.png')
                     plt.close()
                     '''
-                        print(len(aligned_idx), aligned_idx)
-                    exit()
+                    print(len(aligned_idx), aligned_idx)
+                exit()
 
             #print(lm_am_sim.size())
             #print('0'*20)
