980e26c94996e1d720a8c069282f7e8538146852
김도희 <dohee@dohee.local>
2024-02-26 01:19:06 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -427,7 +427,7 @@ def get_params() -> AttributeDict:
             "batch_idx_train": 0,
             "log_interval": 50,
             "reset_interval": 200,
-            "valid_interval": 1000,
+            "valid_interval": 100000,
             # parameters for conformer
             "feature_dim": 80,
             "subsampling_factor": 4,
