1a1d83f70273513732fecd71c42c65bd4234802a
김도희 <dohee@dohee.local>
2024-02-25 16:32:53 +0900

    from local

--- egs/librispeech/ASR/run_mas.sh
+++ egs/librispeech/ASR/run_mas.sh
@@ -8,7 +8,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
-	--max-duration 600 \
+	--max-duration 800 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
 	--num-epochs 30 \
