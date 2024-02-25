069a69c70015e500ed40df53ff916d30c9d5a7f4
김도희 <dohee@dohee.local>
2024-02-26 00:41:38 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -414,7 +414,7 @@ class Conformer(Transformer):
                     else:
                         x = torch.cat([x, am[:pad_mask[i],]], dim=0)
 
-                    print(pad_mask[i], len(lm_target[i]))
+                    print(pad_mask[i].item(), len(lm_target[i]))
                 exit()
                 
                 return (x, lm_am_sim, alignment_target), encoder_memory, memory_key_padding_mask
