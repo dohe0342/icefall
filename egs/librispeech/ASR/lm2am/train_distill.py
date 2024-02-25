cc7b6c13ce1392ed13539453e402ef7f352b862f
김도희 <dohee@dohee.local>
2024-02-25 15:59:45 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -753,7 +753,7 @@ def compute_loss(
                 dense_fsa_vec_lm = k2.DenseFsaVec(
                     lm_am_sim,
                     supervision_segments_lm,
-                    allow_truncate=3,
+                    allow_truncate=15,
                 )
             else:
                 dense_fsa_vec_lm = None
