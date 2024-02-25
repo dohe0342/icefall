64a9010201f38234f012afc74e53781b44d82217
김도희 <dohee@dohee.local>
2024-02-26 02:40:29 +0900

    from local

--- egs/librispeech/ASR/lm2am/train_distill.py
+++ egs/librispeech/ASR/lm2am/train_distill.py
@@ -792,6 +792,9 @@ def compute_loss(
             ctc_loss = lm_loss
             distill_loss = torch.tensor([0])
             
+            for n, p in model.module.named_parameters():
+                print(n, p[0][0])
+            
         if not params.interctc and not params.condition and not params.distill and not params.lm_tune:
             if type(nnet_output) == tuple:
                 nnet_output = nnet_output[0]
