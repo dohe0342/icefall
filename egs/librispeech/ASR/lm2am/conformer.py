b860fe1a1352a6db903fd54a2dd5cbc776e911d9
김도희 <dohee@dohee.local>
2024-03-03 21:12:25 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -444,7 +444,7 @@ class Conformer(Transformer):
                 last_filename = None
                 for b in range(lm_am_sim_cp.size(0)):
                     filename = filenames[b]
-                    prob, alignment = lm_am_sim_cp[b][:pad_mask[b],].max(-1)
+                    prob, alignment = lm_am_sim_cp[b][:pad_mask[b], :alignment_lengths[b]].max(-1)
                     print(prob[-3:])
                     align_dict[filename] = alignment.tolist()
                     last_filename = filename
