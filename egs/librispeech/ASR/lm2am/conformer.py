3b496a101611cb72c4e11b34aa1fb3babee6ae09
김도희 <dohee@dohee.local>
2024-03-04 00:29:39 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -148,7 +148,8 @@ class Conformer(Transformer):
         if lm_tune:
             self.lm_tune = ScaledLinear(256, 50257, bias=False)
             self.alignment_dict = {}
-            pickle_list = glob('/home/work/workspace/fairseq/scripts/whale/train-960-alignment/*.pickle')
+            #pickle_list = glob('/home/work/workspace/fairseq/scripts/whale/train-960-alignment/*.pickle')
+            pickle_list = glob('./pickle/*.pickle')
             for pickle_f in pickle_list:
                 with open(pickle_f, 'rb') as rb:
                     rb = pickle.load(rb)
