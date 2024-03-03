bef371aad8d340a06f51cbd4e67cc127cc6a8d7e
김도희 <dohee@dohee.local>
2024-03-03 20:51:11 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -430,9 +430,8 @@ class Conformer(Transformer):
                 
                 if 1:
 p                   alignment_lengths = torch.sum(lm_input["attention_mask"], 1)
-                    alignment_target = [[int(j+1) for j in range(alignment_lengths[i])] for i in range(len(alignment_lengths))]
                     for batch in range(lm_am_sim_cp.size(0)):
-                        print(pad_mask[batch], lm_am_sim_cp[batch].size(), alignment_target[batch])
+                        print(pad_mask[batch], lm_am_sim_cp[batch].size(), alignment_lengths[batch])
                         plt.matshow(lm_am_sim_cp[batch][:pad_mask[batch]].T.cpu().numpy())
                         plt.colorbar()
                         if not os.path.exists(f'./png2'):
