b0efce502f57ef208af15ed9bfa5749c33be8805
김도희 <dohee@dohee.local>
2024-03-10 02:19:03 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -11,14 +11,14 @@ port=$(($RANDOM% 601+12300))
 ./lm2am/train_distill.py \
 	--exp-dir lm2am/$1 \
 	--master-port $port \
-	--full-libri 0 \
+	--full-libri 1 \
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
 	--max-duration 800 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
-	--num-epochs 70 \
+	--num-epochs 40 \
 	--att-rate 0.0 \
 	--num-decoder-layers 0 \
 	--distill True \
