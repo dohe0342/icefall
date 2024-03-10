869aa534a724b4c0e6787e69eb6ed7cc16e26584
김도희 <dohee@dohee.local>
2024-03-10 22:54:25 +0900

    from local

--- egs/librispeech/ASR/lm2am/decode.py
+++ egs/librispeech/ASR/lm2am/decode.py
@@ -355,6 +355,7 @@ def decode_one_batch(
     nnet_output, memory, memory_key_padding_mask = model(feature, supervisions, vis=True, filenames=filenames,)
     if vis:
         codeword = nnet_output[-1]
+
     if type(nnet_output) == tuple:
         nnet_output = nnet_output[0]
 
