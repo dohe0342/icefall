5a9001c72ec6916d60738790ca9e729f31344b11
DoheeKim <dohe0342@DoheeKims-MacBook-Pro.local>
2024-03-11 10:15:09 +0900

    from local

--- egs/librispeech/ASR/lm2am/decode.py
+++ egs/librispeech/ASR/lm2am/decode.py
@@ -973,7 +973,6 @@ def main() -> None:
     args.return_cuts = True
     librispeech = LibriSpeechAsrDataModule(args)
     
-    '''
     dev_clean_cuts = librispeech.dev_clean_cuts()
     dev_other_cuts = librispeech.dev_other_cuts()
     test_clean_cuts = librispeech.test_clean_cuts()
@@ -986,7 +985,6 @@ def main() -> None:
 
     test_sets = ["dev-clean", "dev-other", "test-clean", "test-other"]
     test_dls = [dev_clean_dl, dev_other_dl, test_clean_dl, test_other_dl]
-    '''
     
     '''
     test_clean_cuts = librispeech.test_clean_cuts()
@@ -1003,10 +1001,12 @@ def main() -> None:
     test_dls = [train_clean_dl]
     '''
     
+    '''
     #test_clean_cuts = librispeech.test_clean_cuts()
     test_clean_cuts = librispeech.train_all_pure_cuts()
     #test_clean_cuts = librispeech.train_all_shuf_cuts()
     test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
+    '''
 
     test_sets = ["test-clean"]
     test_dls = [test_clean_dl]
