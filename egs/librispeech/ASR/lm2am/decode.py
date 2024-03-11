5c597e0a23df55264431ceb5441be3638773441c
김도희 <dohee@dohee.local>
2024-03-11 20:05:04 +0900

    from local

--- egs/librispeech/ASR/lm2am/decode.py
+++ egs/librispeech/ASR/lm2am/decode.py
@@ -661,7 +661,7 @@ def decode_dataset(
 
     codewords = sorted(codewords.items(), key=lambda x:x[1], reverse=True)
     for k, v in codewords:
-        print(k, v)
+        print(f"{k}\t{v}")
 
     return results
 
