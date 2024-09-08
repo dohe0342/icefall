7ef9575c528e16c06810b8aaa550e925114fa9f2
dohe0342 <kimdohe1070@gmail.com>
2024-09-08 15:05:21 +0900

    from local

--- egs/librispeech/ASR/run_distill.sh
+++ egs/librispeech/ASR/run_distill.sh
@@ -15,7 +15,7 @@ port=$(($RANDOM% 601+12300))
 	--use-fp16 True \
 	--num-workers 9 \
 	--spec-aug-time-warp-factor 80 \
-	--max-duration 800 \
+	--max-duration 2000 \
 	--world-size ${WORLD_SIZE} \
 	--start-epoch 1 \
 	--num-epochs 40 \
