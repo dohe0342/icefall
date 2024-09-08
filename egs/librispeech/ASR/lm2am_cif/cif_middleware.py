2ce5d670d508d33e53ee0f7fb17c9e4b7aa62037
dohe0342 <kimdohe1070@gmail.com>
2024-09-08 17:48:28 +0900

    from local

--- egs/librispeech/ASR/lm2am_cif/cif_middleware.py
+++ egs/librispeech/ASR/lm2am_cif/cif_middleware.py
@@ -95,6 +95,7 @@ class CifMiddleware(nn.Module):
             proj_input = self.conv_dropout(proj_input)
             sig_input = self.weight_proj(proj_input)
             weight = torch.sigmoid(sig_input)
+            print('1', weight[0][0])
         else:
             sig_input = self.weight_proj(encoder_raw_outputs)
             weight = torch.sigmoid(sig_input)
@@ -103,6 +104,7 @@ class CifMiddleware(nn.Module):
         not_padding_mask = ~encoder_padding_mask
         weight = torch.squeeze(weight, dim=-1) * not_padding_mask.int()  # weight has shape B x T
         org_weight = weight
+        print('2', weight[0][0])
 
         # Apply scaling strategies
         if self.training and self.apply_scaling and target_lengths is not None:
@@ -112,6 +114,7 @@ class CifMiddleware(nn.Module):
                 target_lengths / weight_sum, -1)    # normalize_scalar has shape B x 1
             weight = weight * normalize_scalar
         
+        print('3', weight[0][0])
         # Prepare for Integrate and fire
         batch_size = encoder_raw_outputs.size(0)
         max_length = encoder_raw_outputs.size(1)
