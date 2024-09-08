7cba2c698a97b92ccd55bc7907c721d3b1891516
dohe0342 <kimdohe1070@gmail.com>
2024-09-08 18:02:10 +0900

    from local

--- egs/librispeech/ASR/lm2am_cif/conformer.py
+++ egs/librispeech/ASR/lm2am_cif/conformer.py
@@ -471,10 +471,13 @@ class Conformer(Transformer):
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
             lm_am_sim = 200*lm_am_sim
             lm_am_sim_cp = lm_am_sim.detach().clone()
-
-            lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
-            lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
-            lm_am_sim = lm_am_sim.contiguous()
+            
+            if self.cif:
+                lm_am_sim = F.softmax(lm_am_sim, dim=-1)
+            else:
+                lm_am_sim = F.log_softmax(lm_am_sim, dim=-1)
+                lm_am_sim = F.pad(lm_am_sim, (1, 0, 0, 0, 0, 0), value=np.log(np.e**-1))
+                lm_am_sim = lm_am_sim.contiguous()
             
             if vis:
                 lm_am_sim_cp = F.softmax(lm_am_sim_cp, dim=-1)
