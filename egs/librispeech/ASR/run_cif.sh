57c82066e025bacd16374469ac84e7243ab8ddd0
dohe0342 <kimdohe1070@gmail.com>
2024-09-08 16:50:01 +0900

    from local

--- egs/librispeech/ASR/run_cif.sh
+++ egs/librispeech/ASR/run_cif.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 #    export CUDA_VISIBLE_DEVICES="4,5,6,7"
 #fi
 port=$(($RANDOM% 601+12300))
-./lm2am_cif/train_distill.py \
+./lm2am_cif/train_cif.py \
 	--exp-dir lm2am_cif/$1 \
 	--master-port $port \
 	--wandb False \
