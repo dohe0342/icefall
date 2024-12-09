5bfe8413e76f86dbed7b14149ccdbc03a74a64d4
dohe0342 <kimdohe1070@gmail.com>
2024-12-10 02:05:58 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -465,10 +465,8 @@ class Conformer(Transformer):
 
             #lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
             if 1:
-                #am_output = am_output.unsqueeze(2)
-                #lm_output = lm_output.unsqueeze(1)
-                am_output = am_output.expand(-1, -1, lm_output.size(1), -1)
-                lm_output = lm_output.expand(-1, am_output.size(1), -1, -1)
+                am_output = am_output.unsqueeze(2)
+                lm_output = lm_output.unsqueeze(1)
                 lm_am_sim = torch.sqrt(torch.sum((am_output - lm_output) ** 2, dim=-1))
             #lm_am_sim = 200*lm_am_sim
             #lm_am_sim_cp = lm_am_sim.detach().clone()
