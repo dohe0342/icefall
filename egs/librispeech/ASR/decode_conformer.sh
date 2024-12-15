8944f522399c575250f3c2f650de1ed1e64d4640
김도희 <dohee@dohee.local>
2024-12-16 01:55:02 +0900

    from local

--- egs/librispeech/ASR/decode_conformer.sh
+++ egs/librispeech/ASR/decode_conformer.sh
@@ -2,7 +2,7 @@ python3 ./conformer_ctc2/decode.py \
 	--exp-dir $1 \
 	--use-averaged-model True \
 	--epoch $2 \
-	--avg 1 \
+	--avg 10 \
 	--max-duration 1200 \
 	--num-decoder-layers 0 \
 	--kernel-size 15 \
