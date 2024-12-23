fcc9fda396a8d920d137ac260f85bd8a0f1b7dc5
김도희 <dohee@dohee.local>
2024-12-23 21:25:57 +0900

    from local

--- egs/librispeech/ASR/run_conformer_large.sh
+++ egs/librispeech/ASR/run_conformer_large.sh
@@ -26,7 +26,7 @@ WORLD_SIZE=2
     --use-fp16 True \
     --num-workers 9 \
     --spec-aug-time-warp-factor 80 \
-    --max-duration 1000 \
+    --max-duration 1500 \
     --world-size ${WORLD_SIZE} \
     --start-epoch 1 \
     --num-epochs 40 \
