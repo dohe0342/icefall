2b049d73f4074d0a58b6013c45b3d3c73ae6684a
김도희 <dohee@dohee.local>
2024-12-23 21:12:59 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -457,7 +457,7 @@ class Conformer(Transformer):
                 #return (x, lm_am_sim, alignment_target), encoder_memory, memory_key_padding_mask
 
             lm_am_sim = torch.bmm(am_output, lm_output.transpose(1, 2))
-            if 0:
+            if 1:
                 am_output = am_output.unsqueeze(2)
                 lm_output = lm_output.unsqueeze(1)
                 lm_am_sim = torch.sqrt(torch.sum((am_output - lm_output) ** 2, dim=-1))
