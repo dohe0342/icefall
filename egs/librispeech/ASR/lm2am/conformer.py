7d30fad54e87309e18bdd5bfaec8ffea56f1c741
김도희 <dohee@dohee.local>
2024-12-23 21:09:37 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -386,7 +386,7 @@ class Conformer(Transformer):
             with torch.no_grad():
                 lm_output = self.lm(**lm_input)
                 lm_output = lm_output['last_hidden_state']
-                lm_output = F.normalize(lm_output, dim=2)
+                #lm_output = F.normalize(lm_output, dim=2)
             
             am_output = encoder_memory.transpose(0, 1).transpose(1, 2)
             
@@ -406,7 +406,7 @@ class Conformer(Transformer):
                     am_output = layer(am_output)
                 am_output = am_output.transpose(1, 2)
                 lm_output = self.lm_decoder[-1](lm_output)
-                am_output = F.normalize(am_output, dim=2)
+                #am_output = F.normalize(am_output, dim=2)
             
             codeword = {} 
             if self.quant is not None:
