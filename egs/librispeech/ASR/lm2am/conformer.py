b916de7468e5181e14046bce63d99e26e2f27690
dohe0342 <kimdohe1070@gmail.com>
2025-01-14 02:25:04 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -52,6 +52,21 @@ from transformers import (
 #    GumbelVectorQuantizer,
 #)
 
+import torch.nn as nn
+
+
+class TransposeLast(nn.Module):
+    def __init__(self, deconstruct_idx=None, tranpose_dim=-2):
+        super().__init__()
+        self.deconstruct_idx = deconstruct_idx
+        self.tranpose_dim = tranpose_dim
+
+    def forward(self, x):
+        if self.deconstruct_idx is not None:
+            x = x[self.deconstruct_idx]
+        return x.transpose(self.tranpose_dim, -1)
+
+
 from cif_middleware import CifMiddleware
 
 
