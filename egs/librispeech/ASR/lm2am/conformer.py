7a07cb050ebe4a78a360851f25cd5f85c501b2d3
dohe0342 <kimdohe1070@gmail.com>
2025-01-14 02:27:53 +0900

    from local

--- egs/librispeech/ASR/lm2am/conformer.py
+++ egs/librispeech/ASR/lm2am/conformer.py
@@ -402,7 +402,7 @@ class Conformer(Transformer):
                     lm_output = lm_output['last_hidden_state']
                 if 0:
                     lm_output = self.lm.wte(lm_input.input_ids)
-                #lm_output = F.normalize(lm_output, dim=2)
+                lm_output = F.normalize(lm_output, dim=2)
             
             am_output = encoder_memory.transpose(0, 1).transpose(1, 2)
             
@@ -422,7 +422,7 @@ class Conformer(Transformer):
                     am_output = layer(am_output)
                 am_output = am_output.transpose(1, 2)
                 lm_output = self.lm_decoder[-1](lm_output)
-                #am_output = F.normalize(am_output, dim=2)
+                am_output = F.normalize(am_output, dim=2)
             
             codeword = {} 
             if self.quant is not None:
