ac5e6c684a0774e26d0da565efa3969e464fd0c2
dohe0342 <kimdohe1070@gmail.com>
2025-01-14 02:03:30 +0900

    from local

--- icefall/checkpoint.py
+++ icefall/checkpoint.py
@@ -78,8 +78,8 @@ def save_checkpoint(
     if isinstance(model, DDP):
         model = model.module
     
-    if exclude:
-        model = {k: v for k, v in model.state_dict().items() if model.state_dict()[k].requires_grad}
+    #if exclude:
+    #    model = {k: v for k, v in model.state_dict().items() if model.state_dict()[k].requires_grad}
 
     checkpoint = {
         "model": model.state_dict(),
